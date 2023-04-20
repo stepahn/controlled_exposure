73209a70a43963e885bcadcb1607de145babfaa7
Stephan Zalewski <stephan@stzal.com>
2023-04-20 08:36:39 +0200

    add test for attr_expose

--- test/controlled_exposure_integration_test.rb
+++ test/controlled_exposure_integration_test.rb
@@ -3,25 +3,29 @@
 require 'test_helper'
 
 class ControlledExposureIntegrationTest < ActionDispatch::IntegrationTest
-  def assert_response_body(path, text, expected)
-    get path, params: { txt: text }
+  def assert_response_body(path, expected, text: nil, params: { txt: text })
+    get path, params: params
     assert_response :success
     assert_equal(expected, response.parsed_body)
   end
 
+  def test_attr_expose
+    assert_response_body('/test_attr_expose', 'TEST_BAR|TEST_BAZ', params: { bar: 'TEST_BAR', baz: 'TEST_BAZ' })
+  end
+
   def test_enforce
-    assert_response_body('/test_enforce', 'TEST_STRING', '')
+    assert_response_body('/test_enforce', '', text: 'TEST_STRING')
   end
 
   def test_enforce_helper
-    assert_response_body('/test_enforce_helper', 'TEST_STRING', '')
+    assert_response_body('/test_enforce_helper', '', text: 'TEST_STRING')
   end
 
   def test_expose
-    assert_response_body('/test_expose', 'TEST_STRING', 'TEST_STRING')
+    assert_response_body('/test_expose', 'TEST_STRING', text: 'TEST_STRING')
   end
 
   def test_expose_ivar
-    assert_response_body('/test_expose_ivar', 'TEST_STRING', 'TEST_STRING')
+    assert_response_body('/test_expose_ivar', 'TEST_STRING', text: 'TEST_STRING')
   end
 end
